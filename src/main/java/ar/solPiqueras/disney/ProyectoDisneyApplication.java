73670b0651e5d6f081a7b540f8eb12faee182146
Sol Piqueras <piquerasmariasol@gmail.com>
2021-12-11 16:04:48 -0300

    Creando proyecto

---
+++ src/main/java/ar/solPiqueras/disney/ProyectoDisneyApplication.java
@@ -0,0 +1,13 @@
+package ar.solPiqueras.disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDisneyApplication.class, args);
+	}
+
+}
