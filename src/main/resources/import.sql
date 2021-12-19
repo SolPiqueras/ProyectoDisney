e181e61bee3c565311f4103e7d3587d3e3e48bf1
Romalde1 <rubenromalde1@hotmail.com>
2021-12-19 14:34:30 +0100

    Añadido el archivo import.sql en src/main/resources. Este archivo contendrá los inserts para meter datos de prueba iniciales automáticamente. Por ahora, solo contiene datos para llenar la tabla de Generos, pero funciona perfectamente y carga los datos sin problema. Las imágenes están alojadas en imgur.com (no son gran cosa pero no se me ocurrió mejor manera de diseñar imágenes de géneros cinematográficos)

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO `disneydb`.`generos` (`imagen_genero`, `nombre_genero`) VALUES ("https://i.imgur.com/QJrcwUM.png", "Fantasía");
+INSERT INTO `disneydb`.`generos` (`imagen_genero`, `nombre_genero`) VALUES ("https://i.imgur.com/KFt3ycw.png", "Animación");
+INSERT INTO `disneydb`.`generos` (`imagen_genero`, `nombre_genero`) VALUES ("https://i.imgur.com/xtQDVD9.png", "Animación 3D");
+INSERT INTO `disneydb`.`generos` (`imagen_genero`, `nombre_genero`) VALUES ("https://i.imgur.com/mXmwBfb.png", "Musical");
+INSERT INTO `disneydb`.`generos` (`imagen_genero`, `nombre_genero`) VALUES ("https://i.imgur.com/Wlf5xbV.png", "Comedia");
+INSERT INTO `disneydb`.`generos` (`imagen_genero`, `nombre_genero`) VALUES ("https://i.imgur.com/UFm3A49.png", "Ciencia Ficción");
+INSERT INTO `disneydb`.`generos` (`imagen_genero`, `nombre_genero`) VALUES ("https://i.imgur.com/tcbdp0w.png", "Terror");
